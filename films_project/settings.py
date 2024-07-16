cb721424ed623f65df6ec9620bc66f77a0e35b7e
Anton Tkachuk <tkacuk2291@gmail.com>
2024-07-16 15:47:29 +0300

    implemneted telegram bot & configure proeject with dotenv

--- films_project/settings.py
+++ films_project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&g%al5%xt)9*#4s)!^-m4*gw7md=of_#2fmel18*yp67=d%$q*"
+load_dotenv()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
